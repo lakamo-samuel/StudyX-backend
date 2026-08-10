d1ccb46d62eb88fdea7ba7e3e4690269706f80d5
Lakamo Samuel <lakamosamuel7@gmail.com>
2026-08-10 03:40:34 +0100

    add member_status column to group_members

---
+++ src/db/migrations/0003_huge_scarlet_spider.sql
@@ -0,0 +1,2 @@
+CREATE TYPE "public"."member_status" AS ENUM('pending', 'approved', 'rejected', 'invited');--> statement-breakpoint
+ALTER TABLE "group_members" ADD COLUMN "status" "member_status" DEFAULT 'approved' NOT NULL;
